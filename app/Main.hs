d71f08813eee50e782c2516fd373e3650e5e854e
Simeon Filipov <ssfilipov@gmail.com>
2015-10-19 22:06:16 +0000

    Add boilerplate function in Database.OLAP

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Database.OLAP
 
 main :: IO ()
-main = someFunc
+main = sendOlapRequest
---
+++ src/Database/OLAP.hs
@@ -0,0 +1,6 @@
+module Database.OLAP
+    ( sendOlapRequest
+    ) where
+
+sendOlapRequest :: IO ()
+sendOlapRequest = error "Still not implemented!"
