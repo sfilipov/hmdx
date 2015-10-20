19f70da2d0c89e329a6e2400807b2c962ae6b4ff
Simeon Filipov <ssfilipov@gmail.com>
2015-10-20 00:47:50 +0000

    Prepare soap body and header

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
 module Main where
 
-import Database.OLAP
+import           Control.Applicative
+import           Data.Text (Text)
+import qualified Data.Text as T
+
+import           Network.SOAP (invokeWS, Transport, ResponseParser(CursorParser))
+import           Network.SOAP.Transport.HTTP (initTransport)
+import           Network.SOAP.Parsing.Cursor (Dict, dictBy, readT, readC)
+import           Text.XML.Cursor (Cursor, ($//), laxElement)
+import           Text.XML.Cursor hiding (element)
+import           Text.XML.Writer (elementA, element)
 
 main :: IO ()
-main = sendOlapRequest
+main = do
+    let ip = "172.28.128.5"
+    let url = "http://" ++ ip ++ "/OLAP/msmdpump.dll"
+    transport <- initTransport url id id
+    response <- getResponse transport
+    print response
+
+getResponse :: Transport -> IO Dict
+getResponse t = invokeWS t discAction header body parser
+  where
+    discAction = "urn:schemas-microsoft-com:xml-analysis:Discover"
+    execAction = "urn:schemas-microsoft-com:xml-analysis:Execute"
+
+    header = elementA "Version"
+        [ ("xmlns", "http://schemas.microsoft.com/analysisservices/2008/engine/100")
+        , ("Sequence", "400")
+        ]
+
+    body = elementA "Discover" [("xmlns","urn:schemas-microsoft-com:xml-analysis")] $ do
+             element "RequestType" $ content "DISCOVER_PROPERTIES"
+             element "Restrictions" $ element "RestrictionList"
+                                    $ element "PropertyName"
+                                    $ content "DbpropMsmdSubqueries"
+             element "Properties" $ element "PropertyList"
