d50597ff07e07a960b78f1927e54e5b8409af5a5
Simeon Filipov <ssfilipov@gmail.com>
2015-10-21 00:36:49 +0000

    executable: Remove unnecessary imports

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,9 @@
 module Main where
 
-import           Control.Applicative
-import           Data.Text (Text)
-import qualified Data.Text as T
-
 import           Network.HTTP.Client (Request, applyBasicAuth)
-import           Network.SOAP (invokeWS, Transport, ResponseParser(CursorParser))
 import           Network.SOAP.Transport.HTTP (initTransport)
-import           Network.SOAP.Parsing.Cursor (Dict, dictBy, readT, readC)
-import           Text.XML.Cursor hiding (element, content)
-import           Text.XML.Writer (elementA, element, content, comment)
 import           Database.OLAP (discoverProperty)
+
 main :: IO ()
 main = do
     let ip = "172.28.128.5"
