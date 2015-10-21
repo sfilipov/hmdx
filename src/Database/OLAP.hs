ca7675a03878cbde4d7f454769a7a7227f63ecd5
Simeon Filipov <ssfilipov@gmail.com>
2015-10-21 00:21:44 +0000

    library: Create discoverProperty function

--- src/Database/OLAP.hs
+++ src/Database/OLAP.hs
@@ -1,6 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Database.OLAP
-    ( sendOlapRequest
+    ( discoverProperty
     ) where
 
-sendOlapRequest :: IO ()
-sendOlapRequest = error "Still not implemented!"
+import           Data.Text (Text)
+import           Network.SOAP (invokeWS, Transport, ResponseParser(CursorParser))
+import           Network.SOAP.Parsing.Cursor (readT)
+import           Text.XML.Cursor hiding (element, content)
+import           Text.XML.Writer (elementA, element, content, comment)
+
+type PropertyName = Text
+
+discoverProperty :: Transport -> PropertyName -> IO Text
+discoverProperty t property = invokeWS t discAction header body (CursorParser parser)
+  where
+    discAction = "urn:schemas-microsoft-com:xml-analysis:Discover"
+
+    header = elementA "Version"
+        [ ("xmlns", "http://schemas.microsoft.com/analysisservices/2008/engine/100")
+        , ("Sequence", "400")
+        ] $ ()
+    body = elementA "Discover" [("xmlns","urn:schemas-microsoft-com:xml-analysis")] $ do
+             element "RequestType" $ content "DISCOVER_PROPERTIES"
+             element "Restrictions" $ element "RestrictionList"
+                                    $ element "PropertyName"
+                                    $ content property
+             element "Properties" $ element "PropertyList" $ ()
+
+    parser :: Cursor -> Text
+    parser cur = readT "Value" row
+                 where row = head $ cur $// laxElement "row"
