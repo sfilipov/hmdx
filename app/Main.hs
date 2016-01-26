db8da202551d83a230bc0ff9b3860cee6ba6478d
Simeon Filipov <ssfilipov@gmail.com>
2016-01-26 19:44:55 +0000

    app: Connect to localhost:8080

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import           Database.OLAP (executeMdx)
 
 main :: IO ()
 main = do
-    let ip = "172.28.128.5"
+    let ip = "localhost:8080"
     let url = "http://" ++ ip ++ "/OLAP/msmdpump.dll"
     let query1 = "SELECT {[Measures].[Internet Freight Cost], [Measures].[Internet Tax Amount]} ON COLUMNS, {[Date].[Fiscal].[Fiscal Year]} ON ROWS FROM [Adventure Works] WHERE {[Sales Territory].[Sales Territory Country].&[United Kingdom]}"
     transport <- initTransport url addAuth id
