1d307dbe7658c8d37b74f5b48e77861ff4f52611
Simeon Filipov <ssfilipov@gmail.com>
2016-02-03 17:29:39 +0000

    app: Calculate number of hierarchies and levels

--- app/Main.hs
+++ app/Main.hs
@@ -6,8 +6,6 @@ import           Network.SOAP.Transport.HTTP (initTransport)
 import           Database.OLAP ( defaultSettings
                                , catalogName
                                , cubeName
-                               , dimensionName
-                               , hierarchyName
                                , mdSchemaDimensions
                                , mdSchemaHierarchies
                                , mdSchemaLevels)
@@ -17,18 +15,18 @@ main = do
     let ip = "localhost:8080"
     let url = "http://" ++ ip ++ "/OLAP/msmdpump.dll"
     let settings = defaultSettings {
-      catalogName = "AdventureWorksDW2012Multidimensional-SE"
-    , cubeName = "Adventure Works"
-    , dimensionName = "[Geography]"
-    , hierarchyName = "[Geography].[Geography]"
-    }
+          catalogName = "AdventureWorksDW2012Multidimensional-SE"
+        , cubeName = "Adventure Works"
+        }
     transport <- initTransport url addAuth id
     putStrLn "!!! List of all dimensions:"
     mdSchemaDimensions transport settings >>= print
-    putStrLn $ "!!! List all hierarchies of " ++ dimensionName settings
-    mdSchemaHierarchies transport settings >>= print
-    putStrLn $ "!!! List all levels of " ++ hierarchyName settings
-    mdSchemaLevels transport settings >>= print
+    putStrLn "!!! Number of hierarchies:"
+    hierarchies <- mdSchemaHierarchies transport settings
+    print $ length hierarchies
+    putStrLn "!!! Number of levels:"
+    levels <- mdSchemaLevels transport settings
+    print $ length levels
 
 addAuth :: Request -> Request
 addAuth = applyBasicAuth "WIN-SSAS\\ReadUser" "Password01"
